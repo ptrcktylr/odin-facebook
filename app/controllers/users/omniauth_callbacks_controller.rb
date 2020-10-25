744bcbcfd92dfedc76846dab3ffa1261f505b031
Patrick Taylor <ptrcktylr@icloud.com>
2020-10-25 13:38:07 -0400

    fixed facebook redirect url

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,15 +7,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      sign_in_and_redirect root_url, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url
+      redirect_to root_url
     end
   end
 
   def failure
-    redirect_to root_path
+    redirect_to root_url
   end
 end
