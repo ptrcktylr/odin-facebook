ff5dc8054837c672fe2328169081478b51caf152
Patrick Taylor <ptrcktylr@icloud.com>
2020-10-26 13:23:25 -0400

    moved jquery files and required them in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("packs/jquery.ui.widget.js")
-require("packs/z.jquery.fileupload.js")
+require("jquery.ui.widget.js")
+require("z.jquery.fileupload.js")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
